c1263364fe9ef61f71813f7f3f547b76d1a53e32
Imad <imadk@richersounds.com>
2025-01-17 14:39:00 +0000

    Update: Fixed CORS issues

--- server/server.js
+++ server/server.js
@@ -5,13 +5,20 @@ import cors from 'cors';
 
 const app = express();
 const port = 3456
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
   }
 });
 
